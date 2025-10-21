778557476d416fe34dabf951d27febfed4fe9c2a
henrrypg <henrry.pulgarin@edunext.co>
2025-10-21 14:21:29 -0500

    chore: solve code issues

--- backend/openedx_ai_extensions/settings/common.py
+++ backend/openedx_ai_extensions/settings/common.py
@@ -3,11 +3,10 @@ Common settings for the openedx_ai_extensions application.
 """
 
 
-def plugin_settings(settings):
+def plugin_settings(settings):  # pylint: disable=unused-argument
     """
     Add plugin settings to main settings object.
 
     Args:
         settings (dict): Django settings object
     """
-    pass
