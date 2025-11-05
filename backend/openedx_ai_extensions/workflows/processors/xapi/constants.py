ac65a9e06fd93adc8130e2923a8764f188f3bcf9
henrrypg <henrry.pulgarin@edunext.co>
2025-11-05 12:12:10 -0500

    feat: add ai workflow event for xApi

---
+++ backend/openedx_ai_extensions/workflows/processors/xapi/constants.py
@@ -0,0 +1,7 @@
+# ai_workflow/constants.py
+
+XAPI_VERB_COMPLETED = "http://adlnet.gov/expapi/verbs/completed"
+COMPLETED = "completed"
+EN = "en"
+
+XAPI_ACTIVITY_AI_WORKFLOW = "https://openedx.org/xapi/activity/ai-workflow"
